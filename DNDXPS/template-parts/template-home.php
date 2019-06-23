bef1e95222c50432cd74af29b775a84bcf56eaf9
Marcelo Chaves <marcelochaves17@gmail.com>
2019-06-23 19:28:14 -0300

    Update website

--- DNDXPS/template-parts/template-home.php
+++ DNDXPS/template-parts/template-home.php
@@ -4,31 +4,25 @@
 
 <div class="container">
     <div class="col-12">
-       <h1> We are a <strong>digital game development studio</strong><br>
-        based in Belo Horizonte, <strong>Brazil</strong>.</h1>
+        <h1> We are a <strong>digital game development studio</strong><br>
+            based in Belo Horizonte, <strong>Brazil</strong>.</h1>
     </div>
-    <div class="container>
-    <div class="row">
-            <ul class="list-inline padding-itens" role="list">
-                 <li class="m1 t-decoration list-inline-item" role="listitem"> 
-                     <a href=" ">  
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/jogo.jpg" alt="Nome do jogo">
-                        <h3>Fuffly Dash</h3>
+    <div class="container">
+        <div class="game_image">
+            <ul class="list-inline">
+                <li>
+                    <a href="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/jogo.jpg" alt="Nome do jogo">
                     </a>
                 </li>
-                <li class="m1 t-decoration list-inline-item" role="listitem"> 
-                     <a href=" ">  
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/jogo.jpg" alt="Nome do jogo">	
-                        <h3>Fuffly Dash</h3>
+                <li>
+                    <a href="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/jogo.jpg" alt="Nome do jogo">
                     </a>
                 </li>
-                <li class="m1 t-decoration list-inline-item" role="listitem"> 
-                     <a href=" ">  
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/jogo.jpg" alt="Nome do jogo">   	
-                        <h3>Fuffly Dash</h3>
+                <li>
+                    <a href="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/jogo.jpg" alt="Nome do jogo">
                     </a>
                 </li>
             </ul>
-    </div>
-
-
